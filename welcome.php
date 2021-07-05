92187fb1f61b2a6390a672e065e87b5a47467bc2
GARIMA32 <61148294+GARIMA32@users.noreply.github.com>
2021-07-05 18:56:28 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,38 +1,31 @@
 <?php
 session_start();
-
-
 if(!isset($_SESSION['logged']) || $_SESSION['logged']!= true)
 {
     header("location:login.php");
     exit;
 }
-
 ?>
-
 <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <title>Welcome - <?php $_SESSION['username']?> </title>
   </head>
   <body>
     <?php require 'partials/nav.php' 
     ?>
-     
     <div class="container">
-    <div class="alert alert-success" role="alert">
-        <h4 class="alert-heading">Welcome - <?php echo $_SESSION['username']?></h4>
-        <p>Hey welcome to the site. You are logged in as <?php echo $_SESSION['username']?></p>
-        <hr>
-        <p class="mb-0">Whenever you need to, be sure to logout <a href="/signup-module/logout.php">using this link</a></p>
-</div>
+        <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading">Welcome - <?php echo $_SESSION['username']?></h4>
+            <p>Hey welcome to the site. You are logged in as <?php echo $_SESSION['username']?></p>
+            <hr>
+            <p class="mb-0">Whenever you need to, be sure to logout <a href="/signup-module/logout.php">using this link</a></p>
+        </div>
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
