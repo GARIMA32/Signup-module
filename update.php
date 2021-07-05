553dddbcf9b3d34d78a86e7944b34b95996c188f
GARIMA32 <61148294+GARIMA32@users.noreply.github.com>
2021-07-05 18:33:06 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,18 +1,13 @@
 <?php
 session_start();
 include('partials/dbconnect.php');
-
-
-
 if(isset($_POST['update']))
 {
     $id = $_POST['id'];
     $username= $_POST['username'];
     $email= $_POST['email'];
-
     $query = "update signup set username='$username', email='$email' where id='$id' ";
     $query_run = mysqli_query($conn, $query);
-
     if($query_run){
         // echo "data updated successfully";
         $_SESSION['status'] = "data updated successfully";
@@ -23,5 +18,4 @@ if(isset($_POST['update']))
         header('Location: login.php');
     }
 }
-
-    ?>
+?>
